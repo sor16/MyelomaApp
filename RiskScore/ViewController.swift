4bbff09fcf68002df0f9493e73df1ed40050492e
Rakari <karirogg@gmail.com>
2016-06-21 21:23:53 +0200

    Fixed warnings

--- RiskScore/ViewController.swift
+++ RiskScore/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
     
